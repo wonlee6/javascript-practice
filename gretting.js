191151b27c9553c648e35730bd0d03db0e0e0769
wonlee6 <wonlee6@gmail.com>
2020-12-18 06:22:18 +0900

    css

--- gretting.js
+++ gretting.js
@@ -21,7 +21,7 @@ function askForName() {
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
-    greeting.innerText = `Hello ${text}`;
+    greeting.innerText = `Hello!😍 ${text}`;
 }
 function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
